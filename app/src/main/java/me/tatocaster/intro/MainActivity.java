9550edfeb4922e3d166457ab381006867ef5f006
Merab Kutalia <kutaliatato@gmail.com>
2016-04-06 22:01:37 +0400

    buttons, views, clicks :tada:

--- app/src/main/java/me/tatocaster/intro/MainActivity.java
+++ app/src/main/java/me/tatocaster/intro/MainActivity.java
@@ -1,60 +1,67 @@
 package me.tatocaster.intro;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "MainActivity";
+    private EditText mEditText;
+    private Button mButton;
+    private TextView mTextView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mEditText = (EditText) findViewById(R.id.editText);
+        mButton = (Button) findViewById(R.id.button);
+        mTextView = (TextView) findViewById(R.id.textView);
 
-        TextView textView = (TextView) findViewById(R.id.hello_world);
+        mButton.setOnClickListener(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-
-        fab.setOnClickListener(new View.OnClickListener() {
+        /*mButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Snackbar", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-
-                Toast.makeText(MainActivity.this, "Toast", Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                String inputValue = "";
+                inputValue = mEditText.getText().toString();
+                mTextView.setText(inputValue);
             }
-        });
+        });*/
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
-            return true;
+    public void onClick(View v) {
+        Log.d(TAG, "onClick() called with: " + "v = [" + v + "]");
+        switch (v.getId()) {
+            case R.id.button:
+                //mTextView.setText("Hello " + mEditText.getText().toString());
+                copyValue();
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    /**
+     * copy value from editext to textview
+     */
+    private void copyValue() {
+        /*String inputValue = "";
+        inputValue = mEditText.getText().toString();*/
+        Log.d(TAG, "copyValue: ");
+        mTextView.setText("Hello " + mEditText.getText().toString());
+    }
+
+    //android:onClick="changeText"
+    /*public void changeText(View view) {
+        String inputValue = "";
+        inputValue = mEditText.getText().toString();
+        mTextView.setText("Hello " + inputValue);
+    }*/
 }
